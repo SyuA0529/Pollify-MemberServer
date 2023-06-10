414c63528573d7782a769ef9c474868c021f4bf3
JH Park <54497150+juliajh@users.noreply.github.com>
2023-06-10 21:51:29 +0900

    fix: try fix cors error

--- src/main/java/dku/cloudcomputing/memberserver/security/WebConfig.java
+++ src/main/java/dku/cloudcomputing/memberserver/security/WebConfig.java
@@ -6,13 +6,13 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //@Configuration
-//public class WebConfig implements WebMvcConfigurer {
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOriginPatterns("*")
-//                .allowedHeaders("*")
-//                .allowedMethods(HttpMethod.GET.name(), HttpMethod.HEAD.name(), HttpMethod.DELETE.name())
-//                .allowCredentials(true);
-//    }
-//}
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.HEAD.name(), HttpMethod.DELETE.name())
+                .allowCredentials(true);
+    }
+}
