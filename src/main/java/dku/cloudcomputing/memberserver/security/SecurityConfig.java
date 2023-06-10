9c21f87e034aee7b098c34164d01fc16d71399b0
JH Park <54497150+juliajh@users.noreply.github.com>
2023-06-10 22:11:54 +0900

    fix: remove cors configure

--- src/main/java/dku/cloudcomputing/memberserver/security/SecurityConfig.java
+++ src/main/java/dku/cloudcomputing/memberserver/security/SecurityConfig.java
@@ -8,9 +8,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
@@ -18,16 +15,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .sessionManagement((sessionManage) -> sessionManage.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .cors(corsConfigurer -> corsConfigurer.configurationSource(request -> {
-                    CorsConfiguration corsConfiguration = new CorsConfiguration();
-                    corsConfiguration.setAllowedOriginPatterns(List.of("*"));
-                    corsConfiguration.setAllowedMethods(List.of("GET", "POST", "DELETE"));
-                    corsConfiguration.setAllowedHeaders(List.of("*"));
-                    corsConfiguration.addExposedHeader("*");
-                    corsConfiguration.setAllowCredentials(true);
-                    return corsConfiguration;
-                }));
+                .sessionManagement((sessionManage) -> sessionManage.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
